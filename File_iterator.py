36580ff0cbb182e64df820e6585baaa4834a3f44
oskar zarnowski <ozarnowski@luc.edu>
2017-03-02 19:39:27 -0600

    file to iterate through

---
+++ File_iterator.py
@@ -0,0 +1,25 @@
+from accession_to_details import openFile
+import argparse
+import os
+
+parser = argparse.ArgumentParser() 
+parser.add_argument("Input_path", help = "path to directory of input files")
+args = parser.parse_args()
+
+
+def detailMaker():
+    o = open("details.txt", 'w')
+    o.write("Accession\tBasic Statistics\tPer base sequence quality\tPer tile sequence quality\tPer sequence quality scores\tPer base sequence content\tPer base N content\tSequence Length Distribution\tSequence Duplication Levels\tOverrepresented sequences\tAdapter Content\tKmer Content")
+    
+    
+    for filename in os.listdir(args.Input_path):
+        if filename.endswith('fastq.gz'):
+            temp = filename.replace('.fastq.gz','')
+            o.write(openFile(temp))
+        elif filename.endswith('fastqc.zip'):
+            temp = filename.replace('_fastqc.zip','')
+            o.write(openFile(temp))
+            
+    o.close()
+    
+detailMaker()
