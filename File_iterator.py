984d5a9e53240a2f84c2e70c959eafa81a59b615
ozarnowski <ozarnowski@luc.edu>
2017-03-02 19:41:00 -0600

    Update File_iterator.py

--- File_iterator.py
+++ File_iterator.py
@@ -9,16 +9,16 @@ args = parser.parse_args()
 
 def detailMaker():
     o = open("details.txt", 'w')
-    o.write("Accession\tBasic Statistics\tPer base sequence quality\tPer tile sequence quality\tPer sequence quality scores\tPer base sequence content\tPer base N content\tSequence Length Distribution\tSequence Duplication Levels\tOverrepresented sequences\tAdapter Content\tKmer Content")
+    o.write("Accession\tBasic Statistics\tPer base sequence quality\tPer tile sequence quality\tPer sequence quality scores\tPer base sequence content\tPer base N content\tSequence Length Distribution\tSequence Duplication Levels\tOverrepresented sequences\tAdapter Content\tKmer Content\n")
     
     
     for filename in os.listdir(args.Input_path):
         if filename.endswith('fastq.gz'):
             temp = filename.replace('.fastq.gz','')
-            o.write(openFile(temp))
+            o.write(openFile(temp) + '\n')
         elif filename.endswith('fastqc.zip'):
             temp = filename.replace('_fastqc.zip','')
-            o.write(openFile(temp))
+            o.write(openFile(temp) + '\n')
             
     o.close()
     
