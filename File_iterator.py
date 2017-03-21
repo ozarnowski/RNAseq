764e28860eddfbc517262bfbae07a25f6c07b8f7
Abdul Zakkar <azakkar@luc.edu>
2017-03-20 22:37:29 -0500

    fixed files accession_to_details and File_iterator to incorporate flexbar

--- File_iterator.py
+++ File_iterator.py
@@ -12,7 +12,11 @@ def detailMaker():
     o.write("Accession\tBasic Statistics\tPer base sequence quality\tPer tile sequence quality\tPer sequence quality scores\tPer base sequence content\tPer base N content\tSequence Length Distribution\tSequence Duplication Levels\tOverrepresented sequences\tAdapter Content\tKmer Content\n")
     
     
+<<<<<<< HEAD
+    for filename in os.listdir(args.Input_path)[:5]: #take of [:10] to do every file
+=======
     for filename in os.listdir(args.Input_path):#[:10]: take of [:10] to do every file
+>>>>>>> d131117195f5704f8fc2f92197b3ab76db817d3b
         if filename.endswith('fastq.gz'):
             temp = filename.replace('.fastq.gz','')
             o.write(openFile(temp, args.Input_path) + '\n')
--- accession_to_details.py
+++ accession_to_details.py
@@ -1,19 +1,23 @@
 import os
 
 def openFile(accession, path):
-    os.system("scp " + path +  accession + ".fastq.gz .")
-    os.system("fastqc " + accession + ".fastq.gz")
-    os.system("rm " + accession + "_fastqc.html")
-    os.system("rm " + accession + ".fastq.gz")
-    os.system("unzip " + accession + "_fastqc.zip")
-    os.system("rm " + accession + "_fastqc.zip")
-    os.chdir(accession + "_fastqc")
-    f = open("summary.txt", 'r')
-    s = f.readlines()
+    os.system("scp " + path + accession + ".fastq.gz .") #get the file from the path to work with
+    os.system("gunzip " + accession + ".fastq.gz") #unzip it for flexbar
+    os.system("flexbar -r " + accession + ".fastq --pre-trim-left 13") #run flexbar on it
+    os.system("mv flexbar.fastq " + accession + "_flexbar.fastq") #rename the file
+    os.system("fastqc " + accession + "_flexbar.fastq --limits limits_edited.txt") #run fastqc on it
+    #os.system("rm " + accession + "_flexbar_fastqc.html") #remove html
+    os.system("rm " + accession + ".fastq") #remove fastq
+    os.system("rm " + accession + "_flexbar.fastq") #remove flexbar output
+    os.system("unzip " + accession + "_flexbar_fastqc.zip") #unzip fastqc output
+    os.system("rm " + accession + "_flexbar_fastqc.zip") #remove zipped file
+    os.chdir(accession + "_flexbar_fastqc") #go into unzipped folder
+    f = open("summary.txt", 'r') #find summary text
+    s = f.readlines() #read it
     f.close()
     os.chdir("..")
-    os.system("rm -r " + accession + "_fastqc")
+    os.system("rm -r " + accession + "_flexbar_fastqc")#remove unzipped folder
     output = accession
     for line in s:
-        output += '\t' + line.split("\t")[0]
+        output += '\t' + line.split("\t")[0]#create output from summary text
     return output
