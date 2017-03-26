7705f595b236a96d608d72d964436faaa42d8c23
oskar zarnowski <ozarnowski@luc.edu>
2017-03-26 14:12:06 -0500

    added parser tested it and it works

--- details_parse.py
+++ details_parse.py
@@ -8,3 +8,17 @@ args = parser.parse_args()
 
 detailMaker(args.Input_path)
 
+def parser():
+    o = open('details.txt','r')
+    f = open('final_cut.txt','w')
+    for line in o:
+        if not line.startswith("Accession"):
+            temp = line.split('\t')
+            del temp[-1]
+            del temp[9]
+            del temp[3]
+            
+            if temp.count("FAIL")==0:
+                f.write(temp[0] + "\n")
+
+parser()
