c1fe21624f09e8bc2af4a9c84bde75caa6b0e5a1
oskar zarnowski <ozarnowski@luc.edu>
2017-03-27 15:50:40 -0500

    fixed details_parse.py to run as paired end

--- details_parse.py
+++ details_parse.py
@@ -9,16 +9,20 @@ args = parser.parse_args()
 detailMaker(args.Input_path)
 
 def parser():
-    o = open('details.txt','r')
+    o = open('details.txt','r').readlines()
     f = open('final_cut.txt','w')
-    for line in o:
-        if not line.startswith("Accession"):
-            temp = line.split('\t')
-            del temp[-1]
-            del temp[9]
-            del temp[3]
+    o = o[1:]
+    for i in range(len(o)/2):
+        temp1 = o[i*2].split('\t')
+        temp2 = o[i*2+1].split('\t')
+        del temp1[-1]
+        del temp1[9]
+        del temp1[3]
+        del temp2[-1]
+        del temp2[9]
+        del temp2[3]    
             
-            if temp.count("FAIL")==0:
-                f.write(temp[0] + "\n")
+            if temp1.count("FAIL") == 0 and temp2.count("FAIL") == 0:
+                f.write(temp1[0] + "\n" + temp2[0] + "\n")
 
 parser()
